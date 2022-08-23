9f3afb4df31df89c02b33a626eb04e51626f34ad
Xingchang Huang <xhuang@mpi-inf.mpg.de>
2022-08-23 16:42:06 +0200

    testing cuda on cluster

--- learn_cuda/RandomCode/src/cmake_cuda.sh
+++ learn_cuda/RandomCode/src/cmake_cuda.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres gpu:1
 #SBATCH -o slurm_outputs/slurm-%j.out
 
-cuda_version=10.0
+cuda_version=10.2
 export PATH=/usr/lib/cuda-${cuda_version}/bin/:${PATH}
 export LD_LIBRARY_PATH=/usr/lib/cuda-${cuda_version}/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 export CUDA_PATH=/usr/lib/cuda-${cuda_version}/
