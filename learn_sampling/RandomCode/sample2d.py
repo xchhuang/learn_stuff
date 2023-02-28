62dc0458b722a827d0595c4a43955814a3b9d944
Xingchang Huang <xhuang@mpi-inf.mpg.de>
2023-02-28 15:49:37 +0800

    adding sample2d

--- learn_sampling/RandomCode/sample2d.py
+++ learn_sampling/RandomCode/sample2d.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sampling_utils import ImportanceSamples
+import  sampling_utils
 
 
 def gauss2d(x, mu, sigma):
@@ -10,23 +10,39 @@ def gauss2d(x, mu, sigma):
 
 def main():
     res = 100
-    x = np.linspace(-1, 1, res)
-    y = np.linspace(-1, 1, res)
+    x = np.linspace(-3, 3, res)
+    y = np.linspace(-3, 3, res)
     X, Y = np.meshgrid(x, y)
     grid = np.stack([X, Y], axis=-1).reshape(res * res, -1)
     y = gauss2d(grid, 0, 1).reshape(res, res)
     print(y.shape, y.min(), y.max())
     
     den = (y - y.min()) / (y.max() - y.min())
-    pts = ImportanceSamples(den, 1000)
+    num_points = 1000
+    pts_white = sampling_utils.ImportanceSamples(den, num_points)
+    pts_blue = sampling_utils.ImportanceSamplesBlue(den, num_points)
 
+    pts_white = (pts_white - 0.5) * 6
+    pts_blue = (pts_blue - 0.5) * 6
+    
     plt.figure(1)
-    plt.subplot(121)
+    plt.subplot(131)
+    plt.title('Density')
     plt.imshow(y)
-    plt.subplot(122)
-    plt.scatter(pts[:, 0], pts[:, 1], s=0.1)
+    plt.axis('off')
+    plt.subplot(132)
+    plt.title('Random samples')
+    plt.scatter(pts_white[:, 0], pts_white[:, 1], s=0.1)
     plt.gca().set_aspect('equal')
-    plt.show()
+    plt.axis('off')
+    plt.subplot(133)
+    plt.title('Blue noise samples')
+    plt.scatter(pts_blue[:, 0], pts_blue[:, 1], s=0.1)
+    plt.gca().set_aspect('equal')
+    plt.axis('off')
+    # plt.show()
+    plt.savefig('results/sample2d.png', bbox_inches='tight', pad_inches=0, dpi=300)
+    plt.clf()
 
 
 if __name__ == '__main__':
--- learn_sampling/RandomCode/sampling_utils.py
+++ learn_sampling/RandomCode/sampling_utils.py
@@ -1,6 +1,12 @@
 import numpy as np
 import torch
 import bisect
+from tqdm import tqdm
+
+
+def get_rmax(num_points):
+    y = 2.0 * np.sqrt(1.0 / (2.0 * np.sqrt(3.0) * num_points))
+    return y
 
 
 def getCDF(importance):
@@ -51,4 +57,81 @@ def ImportanceSamples(den, num_points):
     # plt.scatter(pts[:, 0], pts[:, 1], s=0.1)
     # plt.gca().set_aspect('equal')
     # plt.show()
+    return pts
+
+
+
+
+
+def ImportanceSamplesBlue(den, num_points):
+    pts = np.random.rand(1, 2)
+
+    # mindist = get_rmax(num_points)
+    mindist = 0.015
+    height, width = den.shape[-2], den.shape[-1]
+    importance = np.random.rand(height * width)
+
+    for i in range(importance.shape[0]):
+        importance[i] = den[int(i / width), i % width]
+
+    cum = getCDF(importance)
+
+    for i in tqdm(range(num_points)):
+
+        reject = True
+        failcount = 0
+
+        while reject:
+
+            pt = np.random.rand(2)
+            num = randCDF(cum, importance.shape[0])
+            pt[0] += float(num % width)
+            pt[1] += float(num / width)
+            pt[0] /= width
+            pt[1] /= height
+            pt = pt[None, ...]
+
+            if np.min(np.sum((pts - pt) ** 2, 1)) > mindist * mindist:
+                pts = np.vstack((pts, pt))
+                failcount = 0
+                reject = False
+            else:
+                failcount += 1
+
+            if failcount > 100:
+                mindist = 0.999 * mindist
+                failcount = 0
+                reject = False
+
+    pts = pts.astype(np.float32)
+    pts[:, 1] = 1 - pts[:, 1]
+    return pts
+
+
+
+def blue(npts, mindist=0):
+    """
+    Generates well spaced points with an adaptative dart throw method
+    Output is similar to blue noise
+
+    Parameters
+    ----------
+    npts : number of points to generate
+    mindist : initial minimal accepted distance between points
+    """
+    if mindist == 0:
+        mindist = get_rmax(npts)
+    failcount = 0
+    pts = np.random.rand(1, 2)
+    while np.size(pts, 0) < npts:
+        pt = np.random.rand(1, 2)
+        if np.min(np.sum((pts - pt) ** 2, 1)) > mindist * mindist:
+            pts = np.vstack((pts, pt))
+            failcount = 0
+        else:
+            failcount += 1
+
+        if failcount > 100:
+            mindist = 0.999 * mindist
+            failcount = 0
     return pts
